8d58ee87fd2286811874eb701b4a3051e636f758
Pooja <pgarla3@lsu.edu>
2025-03-22 16:11:38 -0500

    inital commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,6 @@
-//import Image from "next/image";
+"use client";
+import React from "react";
 
-/**
- * 
- * 
- *  <div className= "grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-        H
-      </div>
- */
 export default function Home() {
   return (
      <div>
